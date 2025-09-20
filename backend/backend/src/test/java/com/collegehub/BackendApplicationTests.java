1f8c7fb169e3bc891ad568ba815ca92627de733f
rohithreddy89 <rohithreddythummala5846@gmail.com>
2025-09-20 13:28:00 +0530

    Initial project setup: Spring Boot backend + React frontend

---
+++ backend/backend/src/test/java/com/collegehub/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
